77b604bb3b92f678d134acfee454b46660363b92
Елизавета Перевалова <lisaperevalova1@gmail.com>
2024-05-08 00:05:34 +0500

    1

---
+++ yatube_api/api/serializers.py
@@ -0,0 +1,35 @@
+# 3 сериализатора по моделям
+from django.shortcuts import get_object_or_404
+from rest_framework import serializers
+
+from posts.models import Group, Post, Comment
+
+
+class GroupSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Group
+        fields = ('id', 'title', 'slug', 'description')
+
+
+class PostSerializer(serializers.ModelSerializer):
+    author = serializers.StringRelatedField(read_only=True)
+
+    class Meta:
+        model = Post
+        fields = ("id", 'text', 'pub_date', 'author', 'image', 'group')
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.StringRelatedField(read_only=True)
+    post = serializers.PrimaryKeyRelatedField(read_only=True)
+
+    class Meta:
+        model = Comment
+        fields = ('id', 'author', 'post', 'text', 'created')
+        read_only_fields = ("post",)
+
+    def get_post(self, ):
+        post_id = self.kwargs.get('post_id')
+        post = get_object_or_404(Post, id=post_id)
+        return post
---
+++ yatube_api/api/urls.py
@@ -0,0 +1,18 @@
+from rest_framework import routers
+from rest_framework.authtoken import views
+
+from django.urls import include, path
+
+from .views import PostViewSet, GroupViewSet, CommentViewSet
+
+app_name = 'api'
+
+router_v1 = routers.DefaultRouter()
+router_v1.register('posts', PostViewSet)
+router_v1.register('groups', GroupViewSet)
+router_v1.register(r'posts/(?P<post_id>\d+)/comments', CommentViewSet)
+
+urlpatterns = [
+    path('v1/api-token-auth/', views.obtain_auth_token),
+    path('v1/', include(router_v1.urls)),
+]
